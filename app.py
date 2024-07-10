234deadaea5afb52a3b2c14321883312cf0fe0f8
Savindu Sihilel <lk.ssihilel@gmail.com>
2024-07-10 13:39:14 +0530

    a few tweaks

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 
 # Function to fetch poster of the movie
 def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
+     url = "".format(movie_id)
      data=requests.get(url)
      data=data.json()
      poster_path = data['poster_path']
